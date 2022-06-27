a8648f59d308aa136edfb4c5670d5dc140cb1092
Hristo Dikov <dikoww7@gmail.com>
2022-06-27 17:56:08 +0300

    Changed action name

--- src/SimpleAPI/Controllers/TestController.cs
+++ src/SimpleAPI/Controllers/TestController.cs
@@ -7,16 +7,9 @@ namespace SimpleAPI.Controllers;
 public class TestController : ControllerBase
 {
 
-    [HttpGet(Name = "GetWeatherForecast")]
+    [HttpGet(Name = "Get")]
     public string Get()
     {
         return "Hristo Dikov";
     }
 }
-
-//
-//  "date": "2022-06-26T11:32:04.1508295+03:00",
-//     "temperatureC": 13,
-//     "temperatureF": 55,
-//     "summary": "Cool"
-//   },
